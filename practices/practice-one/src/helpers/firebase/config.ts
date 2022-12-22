54d00b6818435813e5c15e21e55a3abcf73790b2
Duong <phamtanduongtk@gmail.com>
2022-12-22 09:33:19 +0700

    #3 Fix comment supporter

--- practices/practice-one/src/helpers/firebase/config.ts
+++ practices/practice-one/src/helpers/firebase/config.ts
@@ -1,13 +1,8 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getAuth } from 'firebase/auth'
 import { getDatabase } from 'firebase/database'
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
     apiKey: "AIzaSyDujQ211O3uvRSZFfzG27NG9g9H0mKvfJg",
     authDomain: "blog-site-66302.firebaseapp.com",
