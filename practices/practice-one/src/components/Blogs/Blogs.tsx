05ac3568891f79fb43352b759f388ff290c89482
Duong <phamtanduongtk@gmail.com>
2022-12-23 11:53:20 +0700

    #9 Create list-blog section

--- practices/practice-one/src/components/Blogs/Blogs.tsx
+++ practices/practice-one/src/components/Blogs/Blogs.tsx
@@ -1,7 +1,23 @@
 import "./blogs.css";
 
-function Blogs() {
-  return <section className="list-blog container"></section>;
+import Card, { CardProps } from "components/Card/Card";
+import Grid from "components/Grid/Grid";
+
+type BlogsProps = {
+  data: CardProps[];
+};
+
+function Blogs(props: BlogsProps) {
+  const { data } = props;
+  return (
+    <section className="list-blog container">
+      <Grid col={4}>
+        {data.map((blog, index) => (
+          <Card {...blog} key={index} />
+        ))}
+      </Grid>
+    </section>
+  );
 }
 
 export default Blogs;
