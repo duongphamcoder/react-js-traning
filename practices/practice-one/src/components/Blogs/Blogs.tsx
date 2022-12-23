0f1980964d345f479eb69a3ddd66f5186fd5307b
Duong <phamtanduongtk@gmail.com>
2022-12-23 14:57:53 +0700

    #9 Update code and styles list-blog-section

--- practices/practice-one/src/components/Blogs/Blogs.tsx
+++ practices/practice-one/src/components/Blogs/Blogs.tsx
@@ -10,10 +10,12 @@ type BlogsProps = {
 function Blogs(props: BlogsProps) {
   const { data } = props;
   return (
-    <section className="list-blog container">
+    <section className="list-blog">
       <Grid col={4}>
         {data.map((blog, index) => (
-          <Card {...blog} key={index} />
+          <section>
+            <Card {...blog} key={index} />
+          </section>
         ))}
       </Grid>
     </section>
