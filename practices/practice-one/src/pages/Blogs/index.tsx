1a0c057df1887f9c7d323fe2f854f91837e9455c
Duong <phamtanduongtk@gmail.com>
2022-12-26 14:51:51 +0700

    #9 Update code BlogPage

--- practices/practice-one/src/pages/Blogs/index.tsx
+++ practices/practice-one/src/pages/Blogs/index.tsx
@@ -1,52 +1,14 @@
 import Blogs from "components/Blogs/Blogs";
-import { CardProps } from "components/Card/Card";
 import NewBlog from "components/NewBlog/NewBlog";
-// sample data
-const data: CardProps = {
-  image: "https://www.datocms-assets.com/48401/1627664298-javascript.png?fit=max&w=900",
-  path: "/",
-  timeStamp: "8:31 PM • September 21, 2022",
-  title: "A ‘Stripe for phone plans’: Gigs raises $20M to help any company be an MVNO",
-};
-
-const blogs: CardProps[] = [
-  {
-    image: "https://www.datocms-assets.com/48401/1627664298-javascript.png?fit=max&w=900",
-    path: "/",
-    timeStamp: "8:31 PM • September 21, 2022",
-    title: "A ‘Stripe for phone plans’: Gigs raises $20M to help any company be an MVNO",
-    category: "Javascript",
-  },
-  {
-    image: "https://www.datocms-assets.com/48401/1627664298-javascript.png?fit=max&w=900",
-    path: "/",
-    timeStamp: "8:31 PM • September 21, 2022",
-    title: "A ‘Stripe for phone plans’: Gigs raises $20M to help any company be an MVNO",
-    category: "Javascript",
-    isUser: true,
-  },
-  {
-    image: "https://www.datocms-assets.com/48401/1627664298-javascript.png?fit=max&w=900",
-    path: "/",
-    timeStamp: "8:31 PM • September 21, 2022",
-    title: "A ‘Stripe for phone plans’: Gigs raises $20M to help any company be an MVNO",
-    category: "Javascript",
-  },
-  {
-    image: "https://www.datocms-assets.com/48401/1627664298-javascript.png?fit=max&w=900",
-    path: "/",
-    timeStamp: "8:31 PM • September 21, 2022",
-    title: "A ‘Stripe for phone plans’: Gigs raises $20M to help any company be an MVNO",
-    category: "Javascript",
-  },
-];
+import { blogs, data } from "constants/sample-data";
+import DefaultLayout from "layouts/DefaultLayout/DefaultLayout";
 
 function BlogsPage() {
   return (
-    <>
+    <DefaultLayout>
       <NewBlog data={data} />
       <Blogs data={blogs} />
-    </>
+    </DefaultLayout>
   );
 }
 
