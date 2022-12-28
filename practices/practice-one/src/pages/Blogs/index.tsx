0c8e2e413249251123fa84d260eb63b95c8b5196
Duong <phamtanduongtk@gmail.com>
2022-12-28 11:55:07 +0700

    #64 Create global state

--- practices/practice-one/src/pages/Blogs/index.tsx
+++ practices/practice-one/src/pages/Blogs/index.tsx
@@ -2,8 +2,11 @@ import Blogs from "components/Blogs/Blogs";
 import NewBlog from "components/NewBlog/NewBlog";
 import { blogs, data } from "constants/sample-data";
 import DefaultLayout from "layouts/DefaultLayout/DefaultLayout";
+import { useContext } from "react";
+import { GlobalContext } from "context/Global/globalContext";
 
 function BlogsPage() {
+  const [state, dispath] = useContext(GlobalContext);
   return (
     <DefaultLayout>
       <NewBlog data={data} />
