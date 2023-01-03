3b75ac526cc1547406b0fabe1eddc7c12f430d73
Duong <phamtanduongtk@gmail.com>
2023-01-03 15:00:17 +0700

    #15 Create handle delete blog

--- practices/practice-one/src/components/Blogs/index.tsx
+++ practices/practice-one/src/components/Blogs/index.tsx
@@ -1,7 +1,10 @@
-import './blogs.css';
-
-import Card, { CardProps } from 'components/Card/Card';
+import { MouseEvent } from 'react';
+import { deleteDoc, doc } from 'firebase/firestore';
+import { firestore } from 'databases/firebase-config';
+import { Collection } from 'constants/firebase';
 import Grid from 'components/Grid';
+import Card, { CardProps } from 'components/Card/Card';
+import './blogs.css';
 
 type BlogsProps = {
     data: CardProps[];
@@ -10,11 +13,25 @@ type BlogsProps = {
 const Blogs = (props: BlogsProps) => {
     const { data } = props;
 
+    const handleDeleteBlog = async (blog_id: string) => {
+        const value = confirm('Are you sure, you want to delete');
+        if (value) {
+            await deleteDoc(doc(firestore, Collection.BLOG, blog_id));
+        }
+    };
+
     return (
         <section className="list-blog">
             <Grid col={4}>
                 {data.map((blog, index) => (
-                    <Card {...blog} key={blog.id} />
+                    <Card
+                        {...blog}
+                        key={blog.id}
+                        onDelete={(event: MouseEvent) => {
+                            event.preventDefault();
+                            handleDeleteBlog(blog.id);
+                        }}
+                    />
                 ))}
             </Grid>
         </section>
