f78eaf89c2762a25c2f6ca4f2971c487f839ba1c
Duong <phamtanduongtk@gmail.com>
2023-01-04 17:37:08 +0700

    #8 Create notification component

---
+++ practices/practice-one/src/components/Notification/index.tsx
@@ -0,0 +1,18 @@
+import './notification.css';
+
+export type Notofication = {
+    message?: string;
+    variant: 'success' | 'error';
+};
+
+const Notofication = (props: Notofication) => {
+    const { message, variant } = props;
+    return (
+        <section className={`notification-wrapper ${variant}`}>
+            <p className="title">{message}</p>
+            <span className="icon">&#10539;</span>
+        </section>
+    );
+};
+
+export default Notofication;
